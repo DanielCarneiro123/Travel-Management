f4f3354f3f4844176c2117a9d044a2068a94f2c3
DanielCarneiro123 <92723426+DanielCarneiro123@users.noreply.github.com>
2023-05-11 22:12:58 +0100

    Add files via upload

---
+++ src/Graph.cpp
@@ -0,0 +1,134 @@
+//
+// Created by Daniel on 04/05/2023.
+//
+
+#include <fstream>
+#include <mmcobj.h>
+#include "Graph.h"
+
+using namespace std;
+
+int Graph::getNumVertex() const {
+    return vertexSet.size();
+}
+
+std::vector<Vertex *> Graph::getVertexSet() const {
+    return vertexSet;
+}
+
+/*
+ * Auxiliary function to find a vertex with a given content.
+ */
+Vertex * Graph::findVertex(const int &id) const {
+    for (auto v : vertexSet)
+        if (v->getId() == id)
+            return v;
+    return nullptr;
+}
+
+/*
+ * Finds the index of the vertex with a given content.
+ */
+int Graph::findVertexIdx(const int &id) const {
+    for (unsigned i = 0; i < vertexSet.size(); i++)
+        if (vertexSet[i]->getId() == id)
+            return i;
+    return -1;
+}
+/*
+ *  Adds a vertex with a given content or info (in) to a graph (this).
+ *  Returns true if successful, and false if a vertex with that content already exists.
+ */
+bool Graph::addVertex(const int &id) {
+    if (findVertex(id) != nullptr)
+        return false;
+    vertexSet.push_back(new Vertex(id));
+    return true;
+}
+
+/*
+ * Adds an edge to a graph (this), given the contents of the source and
+ * destination vertices and the edge weight (w).
+ * Returns true if successful, and false if the source or destination vertex does not exist.
+ */
+bool Graph::addEdge(const int &sourc, const int &dest, double w) {
+    auto v1 = findVertex(sourc);
+    auto v2 = findVertex(dest);
+    if (v1 == nullptr || v2 == nullptr)
+        return false;
+    v1->addEdge(v2, w);
+    return true;
+}
+
+bool Graph::addBidirectionalEdge(const int &sourc, const int &dest, double w) {
+    auto v1 = findVertex(sourc);
+    auto v2 = findVertex(dest);
+    if (v1 == nullptr || v2 == nullptr)
+        return false;
+    auto e1 = v1->addEdge(v2, w);
+    auto e2 = v2->addEdge(v1, w);
+    e1->setReverse(e2);
+    e2->setReverse(e1);
+    return true;
+}
+
+void deleteMatrix(int **m, int n) {
+    if (m != nullptr) {
+        for (int i = 0; i < n; i++)
+            if (m[i] != nullptr)
+                delete [] m[i];
+        delete [] m;
+    }
+}
+
+void deleteMatrix(double **m, int n) {
+    if (m != nullptr) {
+        for (int i = 0; i < n; i++)
+            if (m[i] != nullptr)
+                delete [] m[i];
+        delete [] m;
+    }
+}
+
+Graph::~Graph() {
+    deleteMatrix(distMatrix, vertexSet.size());
+    deleteMatrix(pathMatrix, vertexSet.size());
+}
+
+void Graph::initialize(const string &filename) {
+
+    fstream file;
+    file.open("../input/" + filename, ios::in);
+    if (!file)
+    {
+        cerr << "Error: file " << filename << " not found" << endl;
+        return;
+    }
+    if (file.is_open())
+    {
+        string numNodes, InitialNode, DestinyNode, capacity, duration;
+        getline(file, numNodes, ' ');
+        this->addVertex(stoi(numNodes)+1);
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        while (!file.eof())
+        {
+            getline(file, InitialNode, ' ');
+            getline(file, DestinyNode, ' ');
+            getline(file, capacity, ' ');
+            getline(file, duration);
+            try {
+                this->addEdge(stoi(InitialNode), stoi(DestinyNode), stoi(capacity));
+            } catch (const exception &e) {
+                return;
+            }
+        }
+        file.close();
+    }
+}
+
+void tsp(int currPath, int currDist, int minDist, int currInd, Graph g){
+    if (currInd < g.getVertexSet().size()){
+        currDist +=
+    }
+}
+
---
+++ src/Graph.h
@@ -0,0 +1,58 @@
+//
+// Created by Daniel on 04/05/2023.
+//
+
+#ifndef PROJETO2_DA_GRAPH_H
+#define PROJETO2_DA_GRAPH_H
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <algorithm>
+#include "MutablePriorityQueue.h"
+
+#include "VertexEdge.h"
+
+class Graph {
+public:
+    ~Graph();
+    /*
+    * Auxiliary function to find a vertex with a given ID.
+    */
+    Vertex *findVertex(const int &id) const;
+    /*
+     *  Adds a vertex with a given content or info (in) to a graph (this).
+     *  Returns true if successful, and false if a vertex with that content already exists.
+     */
+    bool addVertex(const int &id);
+
+    /*
+     * Adds an edge to a graph (this), given the contents of the source and
+     * destination vertices and the edge weight (w).
+     * Returns true if successful, and false if the source or destination vertex does not exist.
+     */
+    bool addEdge(const int &sourc, const int &dest, double w);
+    bool addBidirectionalEdge(const int &sourc, const int &dest, double w);
+
+    int getNumVertex() const;
+    std::vector<Vertex *> getVertexSet() const;
+protected:
+    std::vector<Vertex *> vertexSet;    // vertex set
+
+    double ** distMatrix = nullptr;   // dist matrix for Floyd-Warshall
+    int **pathMatrix = nullptr;   // path matrix for Floyd-Warshall
+
+    /*
+     * Finds the index of the vertex with a given content.
+     */
+    int findVertexIdx(const int &id) const;
+
+    void initialize(const std::string &filename);
+};
+
+void deleteMatrix(int **m, int n);
+void deleteMatrix(double **m, int n);
+
+
+#endif //PROJETO2_DA_GRAPH_H
